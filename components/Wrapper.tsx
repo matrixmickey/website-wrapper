5686764c764970fd189aa3a47f69f2d9c8bded12
matrixmickey <matrixmickey@gmail.com>
2026-01-25 15:59:48 -0500

    feat: serve user to child applications

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,9 +1,27 @@
 "use client";
 
-import { useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
+import { useEffect, useState } from "react";
 
 export default function Wrapper({authorization}: {authorization: React.ReactNode}) {
-    const [selectedAppUrl, setSelectedAppUrl] = useState("https://college-bowl-pool-328214224548.us-east1.run.app");
+    const [selectedAppUrl, setSelectedAppUrl] = useState(process.env.NEXT_PUBLIC_COLLEGE_BOWL_POOL_URL);
+    
+    const { user, isLoading } = useUser();
+
+    useEffect(() => {
+        if (isLoading) {
+            return;
+        }
+        window.addEventListener("message", (event) => {
+            if (event.origin !== process.env.NEXT_PUBLIC_COLLEGE_BOWL_POOL_URL) {
+                return;
+            }
+            if (event.data !== "requestUser") {
+                return;
+            }
+            event.source?.postMessage(user, { targetOrigin: event.origin });
+        });
+    }, [isLoading]);
 
     return (
         <>
@@ -11,12 +29,12 @@ export default function Wrapper({authorization}: {authorization: React.ReactNode
             <div className="body">
             <div>matrixmickeydotcom</div>
             <select value={selectedAppUrl} onChange={(e) => setSelectedAppUrl(e.target.value)}>
-                <option value="https://college-bowl-pool-328214224548.us-east1.run.app">College Bowl Pool</option>
+                <option value={process.env.NEXT_PUBLIC_COLLEGE_BOWL_POOL_URL}>College Bowl Pool</option>
             </select>
             </div>
             {authorization}
         </div>
-        <iframe className="body" src={selectedAppUrl}></iframe>
+        {!isLoading && <iframe className="body" src={selectedAppUrl}></iframe>}
         </>
     );
 } 
