0f7bd6564f4f631877567c762fdcce23b46c4276
matrixmickey <matrixmickey@gmail.com>
2026-01-26 15:03:10 -0500

    feat: share cookie across domains instead of sending cookie value to child

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import Authorization from "@/components/Authorization";
 import Wrapper from "@/components/Wrapper";
-import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  const sessionCookie = cookieStore.get("__session");
 
   return (
     <div className="page">
-      <Wrapper authorization={<Authorization />} sessionCookie={sessionCookie?.value} />
+      <Wrapper authorization={<Authorization />} />
     </div>
   );
 }
