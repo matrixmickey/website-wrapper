5f82549b6375b6b0f67af400cb01f95e59b5f518
matrixmickey <matrixmickey@gmail.com>
2026-05-06 10:43:57 -0400

    remove matrixmickeydotcom text from wrapper

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -9,7 +9,6 @@ export default function Wrapper({authorization}: {authorization: React.ReactNode
         <>
         <div className="header">
             <div className="body">
-            <div>matrixmickeydotcom</div>
             <select value={selectedAppUrl} onChange={(e) => setSelectedAppUrl(e.target.value)}>
                 <option value={process.env.NEXT_PUBLIC_COLLEGE_BOWL_POOL_URL}>College Bowl Pool</option>
             </select>
